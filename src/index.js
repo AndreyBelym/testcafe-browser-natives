738e76d367b4858883b8230660795b5d9c22665c
Andrey Belym <belym.a.2105@gmail.com>
2015-10-08 16:59:57 +0300

    Switched to .NET 4; Integrated arrows linting (closes #43, closes #44)

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import open from './api/open';
 import close from './api/close';
 import resize from './api/resize';
 import screenshot from './api/screenshot';
-
+import findWindow from './api/find-window.js';
 
 export default {
     getInstallations,
     open,
     close,
     resize,
-    screenshot
+    screenshot,
+    findWindow
 };
